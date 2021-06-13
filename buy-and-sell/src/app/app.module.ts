e00872a62ccc8ae5e0eee0d4f97224b80e5aae65
lguilhermefp <79280450+lguilhermefp@users.noreply.github.com>
2021-06-13 17:40:22 -0300

    adding services

--- buy-and-sell/src/app/app.module.ts
+++ buy-and-sell/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,11 +25,13 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     MyListingsPageComponent,
     ListingDataFormComponent,
     NavBarComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- buy-and-sell/src/app/listings-page/listings-page.component.ts
+++ buy-and-sell/src/app/listings-page/listings-page.component.ts
@@ -14,7 +14,8 @@ export class ListingsPageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.listings = this.listingsService.getListings();
+    this.listingsService.getListings()
+      .subscribe(listings => this.listings = listings);
   }
 
 }
